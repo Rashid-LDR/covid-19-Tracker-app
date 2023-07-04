380c535525f58ecad5abb99d60af1566775a91d4
Rashid <mrashidali51@gmail.com>
2023-07-03 22:21:53 -0700

    2nd-Main file

--- covid_track_app/src/App.js
+++ covid_track_app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Card from "./Components/Cards/Card";
+// import Chart from "./Components/Chart/Chart";
+// import CountryPicker from "./Components/CountryPicker/CountryPicker";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Card,Chart,CountryPicker} from './Components'
+import Style from './App.module.css';
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className={Style.container}>
+        <h1><Card/></h1>
+        <h1><Chart/></h1>
+        <h1><CountryPicker/></h1>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
