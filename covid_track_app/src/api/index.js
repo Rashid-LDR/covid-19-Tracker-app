5c21ec56e11473f61f615c7587911f0244108762
Rashid <mrashidali51@gmail.com>
2023-07-07 10:54:55 -0700

    dailydata Covid API is added:

--- covid_track_app/src/api/index.js
+++ covid_track_app/src/api/index.js
@@ -1,16 +1,34 @@
 import axios from "axios";
 
 
-
+const url="https://disease.sh/v3/covid-19/all"
 
 export  default async function  fatchData(){
     try{
-        const {data:{cases,deaths,recovered,updated}}=await axios.get('https://disease.sh/v3/covid-19/all');
+        const {data:{cases,deaths,recovered,updated}}=await axios.get(url);
         return {cases,deaths, recovered ,updated}
     }catch(err){
         console.log(err);
     }
 }
 
+export const  fetchDailyData=async ()=>{
+    try{
+        const {data}=await axios.get('https://disease.sh/v3/covid-19/historical/all?lastdays=all');
+        console.log(data);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
 
  
+
+
+
+
+
+
+
+
+
