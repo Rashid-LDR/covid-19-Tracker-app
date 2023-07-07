90212ccf3627896c067a88694ad07b4f00288383
Rashid <mrashidali51@gmail.com>
2023-07-07 04:53:45 -0700

    Fatch data from API than pass to the Card Component

--- covid_track_app/src/App.js
+++ covid_track_app/src/App.js
@@ -1,20 +1,34 @@
 import React from "react";
+
 // import Card from "./Components/Cards/Card";
 // import Chart from "./Components/Chart/Chart";
 // import CountryPicker from "./Components/CountryPicker/CountryPicker";
 
 import {Card,Chart,CountryPicker} from './Components'
 import Style from './App.module.css';
+import fetchData from './api';
 
 
 class App extends React.Component {
-  render(){
+  state={
+    data:{}
+  }
+
+  async componentDidMount(){
+    const fetchedData=await fetchData();
+    this.setState({data:fetchedData})
+  }
+
+
+  render()  {
+    const {data}=this.state;
     return (
       <div className={Style.container}>
-        <h1><Card/></h1>
+
+        <h1><Card data={data}/></h1>
         <h1><Chart/></h1>
         <h1><CountryPicker/></h1>
-        
+      
       </div>
     );
   }
