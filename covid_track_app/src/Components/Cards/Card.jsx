261bc5789bd92f6cf51ead586ecd82e2f82d30f3
Rashid <mrashidali51@gmail.com>
2023-07-07 04:55:33 -0700

    Add Material UI Cards and Display API data inside Cards

--- covid_track_app/src/Components/Cards/Card.jsx
+++ covid_track_app/src/Components/Cards/Card.jsx
@@ -1,12 +1,74 @@
 import React from 'react'
+import {Card,CardContent,Typography,Grid} from "@mui/material"
+import styles from './Cards.module.css'
+import CountUp from 'react-countup';
 
- function Card() {
+
+function Cards({data:{cases,deaths,recovered}}) {
+// console.log(cases)
+if(!cases)
+{
+  return "Loading...";
+}
+if(!deaths)
+{
+  return "Loading...";
+}
+if(!recovered)
+{
+  return "Loading...";
+}
+
   return (
-    <div>
-      <h1>Cards</h1>
+    <div className={styles.container}>
+        <Grid container spacing={3} justify="center">
+          <Grid item component={Card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>Cases</Typography>
+              <Typography  variant='h5'>
+                <CountUp
+                  start={0}
+                  end={cases}
+                  duration={2.5}
+                  separator=','
+                
+                />
+              </Typography>
+              <Typography color="textSecondary">cases</Typography>
+              <Typography  variant='body2'>Number of Cases of COVID-19</Typography>
+            </CardContent>
+          </Grid>
+          <Grid item component={Card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+              <Typography  variant='h5'><CountUp
+                  start={0}
+                  end={deaths}
+                  duration={2.5}
+                  separator=','
+                
+                /></Typography>
+              <Typography color="textSecondary">Real data</Typography>
+              <Typography  variant='body2'>Number of Death of COVID-19</Typography>
+            </CardContent>
+          </Grid>
+          <Grid item component={Card}>
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>Recovery</Typography>
+              <Typography  variant='h5'><CountUp
+                  start={0}
+                  end={recovered}
+                  duration={2.5}
+                  separator=','
+                
+                /></Typography>
+              <Typography color="textSecondary">Real data</Typography>
+              <Typography  variant='body2'>Number of Recovery of COVID-19</Typography>
+            </CardContent>
+          </Grid>
+        </Grid>      
     </div>
   )
 }
 
-
-export default Card;
+export default Cards;
